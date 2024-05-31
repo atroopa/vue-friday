bb0b89ab143070743ada75a32110d1469ee868ba
atroopa <atroopa@gmail.com>
2024-05-31 05:47:42 -0700

    handleEvent(){ test  method in app

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
                                         block p-2.5 dark:bg-gray-700 dark:border-gray-600 
                                       dark:placeholder-gray-400 dark:text-white 
                                       dark:focus:ring-blue-500 dark:focus:border-blue-500">
-    <button class="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button @click="handleEvent" class="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
       Click
     </button>
   </div>
@@ -20,6 +20,11 @@ export default {
   data() {
     return {
       title: 'this is my title'
+    }
+  },
+  methods: {
+    handleEvent(){
+      console.log(this.$refs.name);
     }
   },
 }
