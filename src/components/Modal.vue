247b16f991d981151a3b3785fca5237681395496
atroopa <atroopa@gmail.com>
2024-05-31 08:27:16 -0700

    learned how modify a event

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <!-- ---------- template ---------- -->
 <template>
-<div @click="changeFromBg" class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50">
+<div @click.self="changeFromBg" class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50">
   <div class="mt-20 w-full max-w-sm mx-auto overflow-hidden rounded-lg shadow-md" > 
     <div class="px-4 py-5 bg-white sm:px-6 opacity-100" :class="{sale: theme === 'sale'}">
       <h1 class="text-2xl pt-2 pb-2 font-bold text-red-500">{{ header }}</h1>
