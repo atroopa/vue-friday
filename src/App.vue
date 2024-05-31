dc14b954d58a9a8d9bf61000d319cb7cbf1d694c
atroopa <atroopa@gmail.com>
2024-05-31 08:00:28 -0700

    learn about dynamic theme in vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="flex flex-col items-center justify-center ">
     <h1 class="text-3xl pt-12 pb-5 font-bold text-red-900">{{title}}</h1>
-    <ModalVue :header="header" :text="text" />
+    <ModalVue :header="header" :text="text" theme="sale" />
   </div>
 </template>
 
@@ -20,6 +20,7 @@ import ModalVue from './components/Modal.vue';
 
     data() {
       return {
+        title : 'My Website Title',
         header: 'Sign up for a Giveaway',
         text: 'Grab the Course for Half Price',
       }
