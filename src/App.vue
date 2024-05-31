a7e84f7f09fedbf0681d4be9d500d3016c955a04
atroopa <atroopa@gmail.com>
2024-05-31 12:33:44 -0700

    for today finish damam garm

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@
   <div class="flex flex-col items-center justify-center ">
     <h1 class="text-3xl pt-12 pb-5 font-bold text-red-900">{{title}}</h1>
     <div v-if="showModal">
-      <ModalVue @close="changeModal" :header="header" :text="text" theme="sale" />
+      <ModalVue @close="changeModal"  theme="sale" >
+        <h1 class="text-2xl pt-2 pb-2 font-bold text-red-500">OpenCode Giveaway !</h1>
+        <p>Grab the Course for half price</p>
+
+        <template v-slot:links>           
+          <a href="">sing up</a><br>
+          <a href="">more info</a><br>
+        </template>
+
+      </ModalVue>
     </div>
     <button @click="changeModal" class="bg-red-800 px-3 py-2 rounded-xl text-red-100">Show Modal</button>
   </div>
@@ -24,8 +33,6 @@ import ModalVue from './components/Modal.vue';
     data() {
       return {
         title : 'My Website Title',
-        header: 'Sign up for a Giveaway',
-        text: 'Grab the Course for Half Price',
         showModal: false,
       }
     },
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,14 +1,16 @@
 <!-- ---------- template ---------- -->
+
 <template>
 <div @click.self="changeFromBg" class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50">
   <div class="mt-20 w-full max-w-sm mx-auto overflow-hidden rounded-lg shadow-md" > 
     <div class="px-4 py-5 bg-white sm:px-6 opacity-100" :class="{sale: theme === 'sale'}">
-      <h1 class="text-2xl pt-2 pb-2 font-bold text-red-500">{{ header }}</h1>
-      <p>{{ text }}</p>
+        <slot></slot>
+        <div class="action">
+            <slot name="links"></slot>
+        </div>
     </div>
   </div>
 </div>
-
 </template>
 
 <!-- ---------- script ---------- -->
@@ -16,8 +18,6 @@
 <script>
 export default {
     props: {
-        header: String,
-        text: String,
         theme: String,
     },
     methods: {
