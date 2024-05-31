d2268ad1a89d0d25ef0f53c655c53e564b3695fe
atroopa <atroopa@gmail.com>
2024-05-31 06:05:58 -0700

    make Modal Component

---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<!-- ---------- template ---------- -->
+<template>
+    <div class ="backdrop">
+        <div class="modal">
+            <p>Modal Content</p>
+        </div>
+    </div>
+</template>
+
+<!-- ---------- script ---------- -->
+
+<script>
+export default {
+      
+}
+</script>
+
+<!-- ---------- style ---------- -->
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+</style>
